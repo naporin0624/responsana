1799951a294d13d3c1cbf034aa67f84266535bf0
naporitan <>
2018-12-17 01:57:09 +0900

    import 修正

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from werkzeug.routing import Rule
 from flask_restful import Api
-from sanaButtonJson import getCategory, getContents
+import sanaButtonJson
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
@@ -23,8 +23,8 @@ def error_handler(error):
 
 
 # apiの登録
-api.add_resource(getCategory, "/api/sana/category")
-api.add_resource(getContents, "/api/sana/contents")
+api.add_resource(sanaButtonJson.getCategory, "/api/sana/category")
+api.add_resource(sanaButtonJson.getContents, "/api/sana/contents")
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", debug=True)
