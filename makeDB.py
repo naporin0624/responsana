b8a5867555a3bb26753addeeba654380a0d6fd0a
naporitan <>
2018-12-18 18:46:24 +0900

    makeDB修正

--- makeDB.py
+++ makeDB.py
@@ -10,18 +10,26 @@ import re
 class sanaScraping:
     def __init__(self):
         html = get("https://www.natorisana.love").content.decode()
+        html = self.__removeStr(html)
         # important-notifyまでいらないので削除
         html = html[html.index('<hr style="margin: 1em 0 ;">'):html.
                     index('<footer>')]
+
         re_pattern = re.compile(r"<b>.*?</b>")
-        self.category = list(map(lambda x: x.strip("<b>").strip(
-            "</b>").replace('\u3000', '').replace('\u2026', '').replace('\u201d', '').replace('\u2018', ''),  re.findall(re_pattern, html)))
+
+        self.category = list(
+            map(lambda x: x.strip("<b>").strip("</b>"),
+                re.findall(re_pattern, html)))
         self.mp3_url_list = list(
             map(self.__pickMp3, re.split(re_pattern, html)))[1:]
         self.mp3_name_list = list(map(self.__pickName, self.mp3_url_list))
 
     def getData(self):
         return list(map(lambda x:{"category":x[0], "contents":x[1], "names":x[2]}, zip(self.category, self.mp3_url_list, self.mp3_name_list)))
+
+    def __removeStr(self, x):
+        re_sub = re.compile(r'\\u[0-9]+')
+        return re.sub(re_sub, '', x)
 
     def __pickMp3(self, x):
         html = fromstring(x)
