e9b001819574a9a8b91cf9c94135b64eb2205a01
naporitan <>
2018-12-21 03:51:14 +0900

    a

--- makeDB.py
+++ makeDB.py
@@ -45,7 +45,7 @@ class voiceScraping(metaclass=ABCMeta):
 
     #u付き文字を削除するメソッド
     def __removeStr(self, x):
-        re_sub = re.compile(r'\\u[0-9]+')
+        re_sub = re.compile(r'\u[0-9]+')
         return re.sub(re_sub, '', x)
 
     #得たデータをカテゴリごとにまとめて返すメソッド
@@ -85,65 +85,48 @@ class sanaScraping(voiceScraping):
     def __pickName(self, x):
         html = fromstring(x)
         name_list = html.xpath("//button/text()")
-        # name_list = list(map(lambda x: x.xpath("//button/text"), x))
-        # name_list = list(map(lambda x: x.split("/")[-1].strip(".mp3"), x))
         return name_list
 
 
 class onyankoScraping(voiceScraping):
+    re_pattern = re.compile(r"<text>.*?</text>")
+
     def easyHandleHTML(self, html):
-        pass
+        start = html.index("<text>【Minecraft】バビニクラフト #6(12/17追加)</text></br>")
+        end = html.index("まだテスト中じゃよ。。。")
+        return html[start:end]
 
     def getCategory(self, html):
-        pass
+        category_list = re.findall(self.re_pattern, html)
+        return list(
+            map(lambda x: x.strip("<text>").strip("</text>"), category_list))
 
     def getURLList(self, html):
-        pass
+        url_list = re.split(self.re_pattern, html)[1:]
+        return list(map(self.__pickMp3, url_list))
 
-    def getMP3List(self, html):
-        pass
+    def getNameList(self, html):
+        button_list = re.split(self.re_pattern, html)[1:]
+        mp3_name_list = list(map(self.__pickName, button_list))
+        return mp3_name_list
 
+    def __pickMp3(self, x):
+        dl_url = "http://onyankopbtn.html.xdomain.jp/sound/"
+        html = fromstring(x)
+        mp3_list = html.xpath("//input/@data-file")
+        mp3_list = list(map(lambda x: dl_url + x + ".mp3", mp3_list))
+        return mp3_list
 
-# class sanaScraping:
-#     def __init__(self):
-#         html = get("https://www.natorisana.love").content.decode()
-#         html = self.__removeStr(html)
-#         # important-notifyまでいらないので削除
-#         html = html[html.index('<hr style="margin: 1em 0 ;">'):html.
-#                     index('<footer>')]
+    def __pickName(self, x):
+        html = fromstring(x)
+        name_list = html.xpath("//input/@value")
+        return name_list
 
-#         re_pattern = re.compile(r"<b>.*?</b>")
-
-#         self.category = list(
-#             map(lambda x: x.strip("<b>").strip("</b>"),
-#                 re.findall(re_pattern, html)))
-#         self.mp3_url_list = list(
-#             map(self.__pickMp3, re.split(re_pattern, html)))[1:]
-#         self.mp3_name_list = list(map(self.__pickName, self.mp3_url_list))
-
-#     def getData(self):
-#         return list(map(lambda x:{"category":x[0], "contents":x[1], "names":x[2]}, zip(self.category, self.mp3_url_list, self.mp3_name_list)))
-
-#     def __removeStr(self, x):
-#         re_sub = re.compile(r'\\u[0-9]+')
-#         return re.sub(re_sub, '', x)
-
-#     def __pickMp3(self, x):
-#         html = fromstring(x)
-#         mp3_list = html.xpath("//button/@data-file")
-#         mp3_list = list(
-#             map(lambda x: "https://www.natorisana.love/sounds/" + x + ".mp3",
-#                 mp3_list))
-#         return mp3_list
-
-#     def __pickName(self, x):
-#         name_list = list(map(lambda x: x.split("/")[-1].strip(".mp3"), x))
-#         return name_list
 
 if __name__ == "__main__":
     MONGO_URL = 'mongodb://heroku_6vdmtkbn:ng692uvshhevmr483e7v2ua0b1@ds135724.mlab.com:35724/heroku_6vdmtkbn'
     sanaButtonURL = "https://www.natorisana.love"
-    onyankoButtonURL = ""
+    onyankoButtonURL = "http://onyankopbtn.html.xdomain.jp"
 
     client = MongoClient(MONGO_URL)
     db = client[urlparse(MONGO_URL).path[1:]]
@@ -159,11 +142,11 @@ if __name__ == "__main__":
         }
         sana.update(selectJson, setJson, upsert=True)
 
-    # for data in onyankoScraping(onyankoButtonURL).getData():
-    #     selectJson = {"category": data["category"]}
-    #     setJson = {
-    #         "category": data["category"],
-    #         "contents": data["contents"],
-    #         "names": data["names"]
-    #     }
-    #     onyanko.updata(selectJson, setJson, upsert=True)
+    for data in onyankoScraping(onyankoButtonURL).getData():
+        selectJson = {"category": data["category"]}
+        setJson = {
+            "category": data["category"],
+            "contents": data["contents"],
+            "names": data["names"]
+        }
+        onyanko.update(selectJson, setJson, upsert=True)
---
+++ onyankoButtonJson.py
@@ -0,0 +1,64 @@
+from requests import get
+from lxml.html import *
+from urllib.parse import urljoin, urlparse
+import os
+from flask_restful import Resource, reqparse
+# from pydub import AudioSegment
+from pymongo import MongoClient
+MONGO_URL = 'mongodb://heroku_6vdmtkbn:ng692uvshhevmr483e7v2ua0b1@ds135724.mlab.com:35724/heroku_6vdmtkbn'
+client = MongoClient(MONGO_URL)
+db = client[urlparse(MONGO_URL).path[1:]]
+co = db.responyanko
+
+
+class getCategory(Resource):
+    def get(self):
+        # print("start get category")
+        res = {"categorylist": sorted([obj["category"] for obj in co.find()])}
+        return res
+
+
+class getContentsNames(Resource):
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument("category", required=True)
+
+    def get(self):
+        # print("start get contents")
+        args = self.parser.parse_args()
+        # print("category:", args["category"])
+        name_list = [
+            obj["names"] for obj in co.find({
+                "category": args["category"]
+            })
+        ][0]
+        name_list.sort()
+        # data = list(map(self.__makeDict, urllist))
+        res = {"voicelist": name_list}
+        return res
+
+
+class getContentsURL(Resource):
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument("category", required=True)
+    parser.add_argument("name", required=True)
+
+    def get(self):
+        args = self.parser.parse_args()
+        record = [x for x in co.find({"category": args["category"]})][0]
+        idx = record["names"].index(args["name"])
+        url = record["contents"][idx]
+        res = {"voiceurl": url}
+        return res
+
+
+if __name__ == "__main__":
+    from urllib.parse import urljoin, urlparse
+    from pymongo import MongoClient
+    MONGO_URL = 'mongodb://heroku_6vdmtkbn:ng692uvshhevmr483e7v2ua0b1@ds135724.mlab.com:35724/heroku_6vdmtkbn'
+    client = MongoClient(MONGO_URL)
+    db = client[urlparse(MONGO_URL).path[1:]]
+    co = db.responsana
+    record = [x for x in co.find({"category": "ぽんぽこ24 リターンズ（神対応 名取さな）"})][0]
+    idx = record["names"].index('まな板！？')
+    url = record["contents"][idx]
+    print(url)
